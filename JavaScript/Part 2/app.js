5f35715a164d5ab0d54ab576dc49b44100398f31
Dimple53 <dimplegautam53@gmail.com>
2025-07-24 15:55:17 +0200

    comparison operator

--- JavaScript/Part 2/app.js
+++ JavaScript/Part 2/app.js
@@ -9,4 +9,20 @@ let eraserPrice = 5;
 // let output = "The total price is: " + (pencilPrice + eraserPrice) + " Rupees."
 let output = `The total price is : ${pencilPrice + eraserPrice} Rupees. `//back ticks
 console.log(output)
-// console.log("The total price is:", pencilPrice + eraserPrice, "Rupees.") 
+// console.log("The total price is:", pencilPrice + eraserPrice, "Rupees.")
+
+//Unary operators
+let a = 10;
+let b = 5;
+console.log(a++);//10
+console.log(++a);//12
+
+//comaparison operators
+let age = 18;
+console.log(age > 18);//false
+
+let n = 5;
+let str = '5';
+console.log(typeof str);
+console.log(n==str);//true becoz it ignores the type in equal to operator.
+console.log(n===str);//false becoz it compares the type also along with the value in triple equal to operator.
