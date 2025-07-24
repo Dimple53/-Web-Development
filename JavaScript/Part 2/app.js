61b7228ff8d2b9952ffc60c0b236adfc131a09cc
Dimple53 <dimplegautam53@gmail.com>
2025-07-24 16:43:56 +0200

    if, else if, else statement

--- JavaScript/Part 2/app.js
+++ JavaScript/Part 2/app.js
@@ -45,3 +45,18 @@ if (lightColor === "green") {
   // console.log("Go light color is " + lightColor);
   console.log(`Go light color is ${lightColor}`);
 }
+
+//if else if else statements
+let size = "S";
+if (size === "XL") {
+  console.log("price is Rs. 250");
+}
+else if (size === "L") {
+  console.log("price is Rs. 200");
+}
+else if (size === "M") {
+  console.log("price is Rs. 100");
+}
+else { 
+  console.log ("price is Rs. 50");
+}
