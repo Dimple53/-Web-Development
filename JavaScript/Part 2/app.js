1f6124de01411d38b1f8335c5ecc5b6e16f491ea
Dimple53 <dimplegautam53@gmail.com>
2025-07-24 15:32:11 +0200

    template literals

--- JavaScript/Part 2/app.js
+++ JavaScript/Part 2/app.js
@@ -1,5 +1,12 @@
-console.log("Hello world!")
-console.log("Dimple")
-let a = 10;
-let b = 5;
-console.log("sum is :", a+b)
+// console.log("Hello world!")
+// console.log("Dimple")
+// let a = 10;
+// let b = 5;
+// console.log("sum is :", a+b)
+
+let pencilPrice = 10;
+let eraserPrice = 5;
+// let output = "The total price is: " + (pencilPrice + eraserPrice) + " Rupees."
+let output = `The total price is : ${pencilPrice + eraserPrice} Rupees. `//back ticks
+console.log(output)
+// console.log("The total price is:", pencilPrice + eraserPrice, "Rupees.") 
