c4a443d185148bc973d4b45d7930f37b858b4226
Dimple53 <dimplegautam53@gmail.com>
2025-07-25 22:54:45 +0200

    js practice question part1

---
+++ JavaScript/PracticeQuestions/Part 2/app.js
@@ -0,0 +1,29 @@
+let number = 8;
+if (number % 10 == 0) {
+  console.log("good");
+} else {
+  console.log("bad");
+}
+
+//prompts
+// let fname = prompt("enter your name: ");
+// let age = prompt("enter your age");
+// alert(`${fname} is ${age} years old.`);
+
+let str = "apples";
+if ((str[0] == 'A' || str[0] == 'a') && (str.length > 5)) {
+  console.log("golden string");
+} else {
+  console.log("not a golden string");
+}
+
+//Write a program to check if 2 numbers have the same last digit. Eg: 32 and 47852 have the same last digit i.e. 2
+let num1 = 32;
+let num2 = 47852;
+
+if ((num1 % 10) == (num2 % 10)) {
+  console.log("numbers have the same last digit which is", num1 % 10);
+} else {
+    console.log("numbers don't have the same last digit");
+
+}
