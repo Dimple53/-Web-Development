6454a1aa2d6bed90ea1c3df292c83117d5b6e30c
Dimple53 <dimplegautam53@gmail.com>
2025-07-26 21:31:38 +0200

    replace and repeat method

--- JavaScript/Part 3/app.js
+++ JavaScript/Part 3/app.js
@@ -12,3 +12,8 @@ console.log(newMsg);
 let msg1 = "college";
 // console.log(msg1.slice(3))//lege
 console.log(msg1.slice(-2))//7-2=5
+
+//replace and repeat method
+let msg2 = "IloveCoding";
+console.log(msg2.replace("love", "do"));// only first occurence is replaced
+console.log(msg2.repeat(3));
