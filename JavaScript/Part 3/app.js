9f206497c37f6664a38e74687d38860b0ecc51fa
Dimple53 <dimplegautam53@gmail.com>
2025-07-26 22:11:06 +0200

    creating arrays

--- JavaScript/Part 3/app.js
+++ JavaScript/Part 3/app.js
@@ -24,3 +24,20 @@ console.log(name.slice(4, 9));//eCodi -- index 9 excluded
 //Methods with Arguments  - indexOf
 console.log(name.indexOf("odi"));//6
 console.log(name.replace("love", "do"));
+
+//Array 
+let students = ["dimple", "addy", "twinkle"];
+console.log(students);
+console.log(typeof (students));
+
+//how to create arrays
+let marks = [33, 445, 55, 566];
+let names = ["dimple", "addy", "twinkle"];
+let info = ["dimple", 23, 5.6];//mixed array
+
+//empty array
+let newArr = [];
+console.log(newArr.length);
+console.log(info);
+console.log(info[0][2]);//m--- [0]-index, [2]- you want to access 3rd character
+console.log([].length);
