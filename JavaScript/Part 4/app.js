7c6f7c0a61b4f5cb6e69c41e02c8ed776f86aaad
Dimple53 <dimplegautam53@gmail.com>
2025-07-28 22:36:16 +0200

    print all even numbers(2 to 10)

--- JavaScript/Part 4/app.js
+++ JavaScript/Part 4/app.js
@@ -10,11 +10,16 @@
 // console.log(i);
 
 //print odd numbers
-for (let i = 1; i <= 15; i =  i + 2){
-  console.log(i);
-}
-console.log("backwards");
+// for (let i = 1; i <= 15; i =  i + 2){
+//   console.log(i);
+// }
+// console.log("backwards");
 
-for (let i = 15; i >= 1; i =  i - 2){
+// for (let i = 15; i >= 1; i =  i - 2){
+//   console.log(i);
+// }
+
+//print all even numbers(2 to 10)
+for (let i = 2; i <= 10; i =  i + 2){
   console.log(i);
 }
