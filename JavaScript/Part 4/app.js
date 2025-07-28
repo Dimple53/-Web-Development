4dbfd8ed6bf8674b6da93578ac55f1168b47b754
Dimple53 <dimplegautam53@gmail.com>
2025-07-28 22:32:39 +0200

    print odd numbers

--- JavaScript/Part 4/app.js
+++ JavaScript/Part 4/app.js
@@ -1,4 +1,20 @@
 //forloop
-for (let i = 1; i <= 5; i++){
+// for (let i = 1; i <= 5; i++){
+//   console.log(i);
+// }
+
+// for (let i = 10; i >= 1; i--){
+//   console.log(i);
+// }
+// let i = 5;
+// console.log(i);
+
+//print odd numbers
+for (let i = 1; i <= 15; i =  i + 2){
+  console.log(i);
+}
+console.log("backwards");
+
+for (let i = 15; i >= 1; i =  i - 2){
   console.log(i);
 }
