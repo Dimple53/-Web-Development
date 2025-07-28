39b1eec14d4c1637b6340fd4d425a43d571cef86
Dimple53 <dimplegautam53@gmail.com>
2025-07-28 23:16:24 +0200

    break keyword

--- JavaScript/Part 4/app.js
+++ JavaScript/Part 4/app.js
@@ -53,14 +53,26 @@
 
 
 //Favorite Movie
-const favMovie = "avatar";
-let guess = prompt("guess my favorite movie");
-while ((guess != favMovie) && (guess != "quit")) {
-  guess = prompt("wrong guess. please try again");
-}
+// const favMovie = "avatar";
+// let guess = prompt("guess my favorite movie");
+// while ((guess != favMovie) && (guess != "quit")) {
+//   guess = prompt("wrong guess. please try again");
+// }
 
-if (guess == favMovie) {
-  console.log("congrats!!");
-} else {
-  console.log("you quit")
+// if (guess == favMovie) {
+//   console.log("congrats!!");
+// } else {
+//   console.log("you quit")
+// }
+
+//break keyword
+let i = 1;
+
+while (i <= 5) {
+  if (i == 3) {
+    break;
+  }
+  console.log(i);
+  i++;
 }
+console.log("we used break at 3");
