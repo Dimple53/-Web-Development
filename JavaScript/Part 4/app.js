2503c9c856d8018e27a4ce681bf719981b77295f
Dimple53 <dimplegautam53@gmail.com>
2025-07-28 22:57:33 +0200

    while loop

--- JavaScript/Part 4/app.js
+++ JavaScript/Part 4/app.js
@@ -36,8 +36,17 @@
 
 
 //nested for loop
-for (let i = 1; i <= 4; i++){//outer loop
-  for (let j = 1; j <= 3; j++){//inner loop
-    console.log(j);
-  }
+// for (let i = 1; i <= 4; i++){//outer loop
+//   console.log(`outer loop ${i}`);
+//   for (let j = 1; j <= 3; j++){//inner loop
+//     console.log(j);
+//   }
+// }
+
+//while loop
+let i = 0;
+
+while (i <= 20) {
+  console.log(i);
+  i++;
 }
