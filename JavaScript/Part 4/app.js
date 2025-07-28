8641acebc6eb3be420da520499707ace213e1098
Dimple53 <dimplegautam53@gmail.com>
2025-07-28 22:45:56 +0200

    print the multiplication table

--- JavaScript/Part 4/app.js
+++ JavaScript/Part 4/app.js
@@ -20,6 +20,17 @@
 // }
 
 //print all even numbers(2 to 10)
-for (let i = 2; i <= 10; i =  i + 2){
+// for (let i = 2; i <= 10; i =  i + 2){
+//   console.log(i);
+// }
+
+// print the multiplication table for 5
+// for (let i = 5; i <= 50; i = i + 5){
+//   console.log(i);
+// }
+let n = prompt("write your number")
+n = parseInt(n);
+for (let i = n; i <= n*10; i = i + n){
   console.log(i);
 }
+
