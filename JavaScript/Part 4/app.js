3da84428faa80550f268852eb7e2415c567ff09b
Dimple53 <dimplegautam53@gmail.com>
2025-07-28 23:47:31 +0200

    nested loops with nested arrays

--- JavaScript/Part 4/app.js
+++ JavaScript/Part 4/app.js
@@ -78,8 +78,18 @@
 // console.log("we used break at 3");
 
 //loops with arrays
-let fruits = ["mango", "apple", "banana", "litchi", "orange"];
-fruits.push("pineapple")
-for (let i = 0; i < fruits.length; i++){
-  console.log(i, fruits[i]);
+// let fruits = ["mango", "apple", "banana", "litchi", "orange"];
+// fruits.push("pineapple")
+// for (let i = 0; i < fruits.length; i++){
+//   console.log(i, fruits[i]);
+// }
+
+//nested loops with nested arrays
+let heroes = [["ironman", "spiderman", "thor"], ["superman", "wonder", "flash"]];
+
+for (let i = 0; i < heroes.length; i++){
+  console.log(i, heroes[i]);
+  for (let j = 0; j < heroes[i].length; j++){
+    console.log(`j=${j}`, heroes[i][j]);
+  }
 }
