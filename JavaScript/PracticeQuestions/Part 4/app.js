daf03c5266da8c589dbf73753650c3f546513cd9
Dimple53 <dimplegautam53@gmail.com>
2025-07-29 17:51:07 +0200

    print factorial of a number n

---
+++ JavaScript/PracticeQuestions/Part 4/app.js
@@ -0,0 +1,29 @@
+// let arr = [1, 2, 3, 4, 5, 6, 2, 3]
+// let num = 2;
+
+// for (let i = 0; i < arr.length; i++){
+//   if (arr[i] == num) {
+//     arr.splice(i, 1);
+//   }
+//   console.log(arr);
+// }
+
+// let number = 287152;
+// let count = 0;
+// let copy = number;
+// while (copy > 0) {
+//   count++;
+//   copy = Math
+// }
+// console.log(number.length());
+// for (let i = 0; i < number.length; i++){
+  
+// }
+
+//print factorial of a number n
+let n = 5;
+let fact = 1;
+for (let i = 1; i<=n; i++){
+  fact = fact * i;
+  console.log(fact);
+}
