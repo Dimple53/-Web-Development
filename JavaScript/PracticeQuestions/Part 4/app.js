728614f676e5693876e6e0b69c82f3847651d2c9
Dimple53 <dimplegautam53@gmail.com>
2025-07-29 18:55:34 +0200

    js practice question part4

--- JavaScript/PracticeQuestions/Part 4/app.js
+++ JavaScript/PracticeQuestions/Part 4/app.js
@@ -1,24 +1,34 @@
-// let arr = [1, 2, 3, 4, 5, 6, 2, 3]
-// let num = 2;
+//delete all occurrences of element ‘num’ in a given array.
+let arr = [1, 2, 3, 4, 5, 6, 2, 3]
+let num = 2;
 
-// for (let i = 0; i < arr.length; i++){
-//   if (arr[i] == num) {
-//     arr.splice(i, 1);
-//   }
-//   console.log(arr);
-// }
+for (let i = 0; i < arr.length; i++){
+  if (arr[i] == num) {
+    arr.splice(i, 1);
+  }
+  console.log(arr);
+}
 
-// let number = 287152;
-// let count = 0;
-// let copy = number;
-// while (copy > 0) {
-//   count++;
-//   copy = Math
-// }
-// console.log(number.length());
-// for (let i = 0; i < number.length; i++){
-  
-// }
+//find the no of digits in a number.
+let number = 287152;
+let count = 0;
+let copy = number;
+while (copy > 0) {
+  count++;
+  copy = Math.floor(copy / 10);
+}
+console.log(`count is ${count}`);
+
+//find the sum of digits in a number.
+let number1 = 287152;
+let sum = 0;
+let copy1 = number1;
+while (copy1 > 0) {
+  digit = copy1 % 10;
+  sum+=digit;
+  copy1 = Math.floor(copy1 / 10);
+}
+console.log(`sum is ${sum}`);
 
 //print factorial of a number n
 let n = 5;
@@ -29,10 +39,10 @@ for (let i = 1; i<=n; i++){
 console.log(`factorial of ${n} is ${fact}`);
 
 //largest number in an array
-let arr = [23, 45, 12, 34, 44];
+let arr1 = [23, 45, 12, 34, 44];
 let largest = 0;
-for (let i = 1; i<=arr.length; i++){
-  if (largest < arr[i]){
+for (let i = 1; i<=arr1.length; i++){
+  if (largest < arr1[i]){
     largest = arr[i];
   }
 }
