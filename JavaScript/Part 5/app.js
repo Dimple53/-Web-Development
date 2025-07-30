f3c7c7108ae41f81779a7dc252251ea8822aea3f
Dimple53 <dimplegautam53@gmail.com>
2025-07-30 22:44:42 +0200

    object literals

---
+++ JavaScript/Part 5/app.js
@@ -0,0 +1,14 @@
+//creating object literals
+const student = {
+  name: "twinkle",
+  age: 21,
+  marks: 94.4
+};
+console.log(student);
+
+const item = {
+  price: 100.99,
+  discount: 50,
+  colors: ["red", "pink"]
+};
+console.log(item);
