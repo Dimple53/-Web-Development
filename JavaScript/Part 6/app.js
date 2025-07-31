df373d9b39815b602f2f9477aa5d153fd5134ad0
Dimple53 <dimplegautam53@gmail.com>
2025-07-31 22:32:33 +0200

    create a func to roll a dice(1 to 6)

--- JavaScript/Part 6/app.js
+++ JavaScript/Part 6/app.js
@@ -8,3 +8,10 @@ function printName() {
   console.log("dimple gautam");
 }
 printName()
+
+//create a func to roll a dice(1 to 6)
+function rollDice(){
+  let rand = Math.floor(Math.random() * 6) + 1;
+  console.log(rand);
+}
+rollDice();
