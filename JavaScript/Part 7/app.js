29473d34ac8409e932aa5589aed564334311d64a
Dimple53 <dimplegautam53@gmail.com>
2025-08-02 08:28:04 +0200

    this keyword

---
+++ JavaScript/Part 7/app.js
@@ -0,0 +1,18 @@
+//this keyword
+const student = {
+  name: "Twinkle",
+  age: 22,
+  eng: 95,
+  math: 93,
+  phy: 97,
+  getAvg() {
+    console.log(this);//here this means the student object
+    let avg = (this.eng + this.math + this.phy) / 3;
+    console.log(`${this.name} got avg marks= ${avg}`);
+  }
+}
+console.log(student.getAvg());
+
+function getAvg() {
+  console.log(this);//by default value of this keyword is window oject when we defining function in general cases
+}
