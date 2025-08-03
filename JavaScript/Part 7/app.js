275366c4e9b69d45d9c20d1f9e1592fc4922c351
Dimple53 <dimplegautam53@gmail.com>
2025-08-03 15:26:39 +0200

    arrow functions and implicit return in arrow funcions

--- JavaScript/Part 7/app.js
+++ JavaScript/Part 7/app.js
@@ -24,3 +24,19 @@ try {
 } catch {
   console.log("caught an error... a is not defined");
 }
+
+//arrow functions
+const sum = (a, b) => {
+  console.log(a + b);
+}
+
+const cube = (n) => {
+  return n * n * n;
+}
+
+const pow = (a,b) => {
+  return a ** b;
+}
+
+//implicit return in arrow funcions
+const mul = (a,b) =>  a * b;
