e804253ce600dba30c3e72532c370ea9d5806958
Dimple53 <dimplegautam53@gmail.com>
2025-08-04 14:30:42 +0200

    map and filter

--- JavaScript/Part 8/app.js
+++ JavaScript/Part 8/app.js
@@ -32,3 +32,38 @@ let array = [
 array.forEach((student) => {
   console.log(student.marks);
 });
+
+//map and filter
+let num = [1, 2, 3, 4, 5];
+
+let double = num.map((el) => {
+  return el * 2;
+});
+console.log(double);//new array
+
+
+let students = [
+  {
+  name: "aman",
+  marks: 95,
+  },
+  {
+  name: "anjali",
+  marks: 85.9,
+  },
+  {
+  name: "shikha",
+  marks: 93,
+  },
+];
+
+let gpa = students.map((el) => {
+  return el.marks / 10;
+})
+
+//filter
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 6];
+let ans = nums.filter((el) => {
+  // return el % 2 == 0;//even -> true, odd-> false
+  return el < 5;
+})
