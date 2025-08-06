d9f588e08e07066aded36672c3590aafa5d65ce3
Dimple53 <dimplegautam53@gmail.com>
2025-08-06 22:54:41 +0200

    export in files

--- Backend 1(Node.js)/script.js
+++ Backend 1(Node.js)/script.js
@@ -4,7 +4,13 @@
 
 // console.log(process.argv);
 
-let args = process.argv;
- for (let i = 2; i <= args.length; i++){
-  console.log("hello to ",  args[i]);
-}
+// let args = process.argv;
+//  for (let i = 2; i <= args.length; i++){
+//   console.log("hello to ",  args[i]);
+// }
+
+//exports in files
+const math = require("./math");
+
+console.log(math.sum(2, 2));
+console.log(math.PI);
