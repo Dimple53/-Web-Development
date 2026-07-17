9cc7a92bd7bc5dd6fa28b106c39a2d81641a5a36
Dimple53 <dimplegautam09@gmail.com>
2026-07-17 20:52:06 +0200

    using ejs and views directory

--- Backend/Backend3/EJSDir/index.js
+++ Backend/Backend3/EJSDir/index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 
 const port = 8080;
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
 app.listen(port, () => {
-  console.log(`app is listening on theport ${port}`);
+  console.log(`app is listening on the port ${port}`);
 });
