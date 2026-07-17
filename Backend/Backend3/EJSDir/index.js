72239914c7ba34fbe7c25818d726684b8f9bde7c
Dimple53 <dimplegautam09@gmail.com>
2026-07-17 21:13:40 +0200

    passing data to ejs

--- Backend/Backend3/EJSDir/index.js
+++ Backend/Backend3/EJSDir/index.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const port = 8080;
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
   res.render("home");
 });
+
+app.get("/about", (req, res) => {
+  res.send("about");
+});
+
+app.get("/rolldice", (req, res) => {
+  let randomNumber = Math.floor(Math.random() * 6) + 1;
+  res.render("rolldice", { num: randomNumber });
+});
+
 
 app.listen(port, () => {
   console.log(`app is listening on the port ${port}`);
